b4ab8ee1f1999719d7a2b84524be8a1d593d85ab
addis <betsega23@gmail.com>
2022-11-14 14:15:32 +0300

    Add test first

--- src/spec/solver_spec.rb
+++ src/spec/solver_spec.rb
@@ -6,6 +6,11 @@ describe Solver do
         expect(solver.factorial(5)).to eq(120)
     end
 
+    it 'Should return -1 when given negative number' do
+        solver = Solver.new
+        expect(solver.factorial(-5)).to eq(-1)
+    end
+
     it "should return 'olleh' when given 'hello'" do
         solver = Solver.new
         expect(solver.reverse('hello')).to eq('olleh')
@@ -13,6 +18,6 @@ describe Solver do
 
     it 'should return "1, 2, Fizz, 4, Buzz" when given 5' do
         solver = Solver.new
-        expect(solver.fizzbuzz(5)).to eq('1, 2, Fizz, 4, Buzz')
+        expect(solver.fizzbuzz(5)).to eq('Buzz')
     end
 end
